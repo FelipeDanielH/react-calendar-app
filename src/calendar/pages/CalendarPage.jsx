be718cc9f386bfc75ee2d9812b18960e1b5b5190
Felipe Daniel <felipe.daniel.henriquez@gmail.com>
2023-02-18 21:29:07 -0300

    Se agregan configuraciones extra al calendario
    -Se crea el helper 'calendarLocalizer' para cambiar al idioma español los textos del calendario (antiguamente en ingles) que estaban dentro de 'CalendarPage'
    
    -Se crea el helper 'getMessages' para exportar los textos manualmente que el localizer no traduce automaticamente

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,22 +1,9 @@
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-import enUS from 'date-fns/locale/en-US';
-
+import { addHours } from 'date-fns';
 import { Navbar } from "../";
-import {addHours, format, parse, startOfWeek, getDay } from 'date-fns';
-
-const locales = {
-  'en-US': enUS,
-}
-
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-})
+import { localizer, getMessagesES } from '../../helpers';
 
 const events = [{
   title: 'Cumple',
@@ -31,16 +18,32 @@ const events = [{
 }]
 
 export const CalendarPage = () => {
+
+  const eventStyleGetter = ( event, start, end, isSelected ) => {
+    console.log({ event, start, end, isSelected });
+
+    const style = {
+      backgroundColor: '#347Cf7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white'
+    }
+
+    return style;
+  }
+
   return (
     <>
       <Navbar/>
-
       <Calendar
+      culture='es'
       localizer={localizer}
       events={events}
       startAccessor="start"
       endAccessor="end"
       style={{ height: 'calc(100vh - 80px)' }}
+      messages={ getMessagesES() }
+      eventPropGetter={ eventStyleGetter }
     />
     </>
   )
