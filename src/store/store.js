27ff6ebc1857172eb03a3a145dc062294ca58859
Felipe Daniel <felipe.daniel.henriquez@gmail.com>
2023-03-30 14:48:12 -0300

    Agregada configuracion de middleware para evitar un check de serializacion en los eventos

--- src/store/store.js
+++ src/store/store.js
@@ -6,5 +6,8 @@ export const store = configureStore({
   reducer:{
     ui: uiSlice.reducer,
     calendar: calendarSlice.reducer
-  }
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: false
+  })
 })
