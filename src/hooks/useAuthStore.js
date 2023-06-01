d08eee2614c3e8bb82b547191415d8ae18f80c12
Felipe <felipe.daniel.henriquez@gmail.com>
2023-05-31 21:14:29 -0400

    se completa funcion de startLogin y agrega informacion al localStorage... queda pendiente completar la funcion 'clearErrorMessage'

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { calendarApi } from "../api";
+import { onChecking, onLogin } from "../store/auth/authSlice";
 
 export const useAuthStore = () => {
 
@@ -7,13 +8,19 @@ export const useAuthStore = () => {
     const dispatch = useDispatch();
 
     const startLogin = async ({ email, password }) => {
-        console.log({ email, password });
+        dispatch( onChecking() );
 
         try {
-            const resp = await calendarApi.post('/auth', {email, password} );
-            console.log( {resp} );
+            const { data } = await calendarApi.post('/auth', {email, password} );
+            localStorage.setItem( 'token', data.token );
+            localStorage.setItem('token-init-date', new Date().getTime() );
+            dispatch( onLogin({ name: data.name, uid: data.uid}) );
+
         }catch( error){
-            console.log( {error} );
+            dispatch( onLogout('Credenciales incorrectas'))
+            setTimeout( () => {
+                dispatch( clearErrorMessage() );
+            }, 10 );
         }
     }
 
