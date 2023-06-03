489bebf917c7ac0b83aff57b7217cb3ba14a4dad
Felipe <felipe.daniel.henriquez@gmail.com>
2023-06-03 15:59:27 -0400

    se agrega interceptor que setea los tokens de una peticion

--- src/api/calendarApi.js
+++ src/api/calendarApi.js
@@ -8,6 +8,14 @@ const calendarApi = axios.create({
 });
 
 //Todo: configurar interceptores
+calendarApi.interceptors.request.use( config => {
 
+    config.headers = {
+        ...config.headers,
+        'x-token': localStorage.getItem('token')
+    }
+
+    return config;
+});
 
 export default calendarApi;
