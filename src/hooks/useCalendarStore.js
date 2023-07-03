0891e0a2a33a0ba0c4ee18aeb3cb114c990dd1be
Felipe <felipe.daniel.henriquez@gmail.com>
2023-07-03 12:46:49 -0400

    se agrega funcion de eliminar

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -33,12 +33,19 @@ export const useCalendarStore = () => {
         }
 
 
-       
+
 
     }
 
-    const startDeletingEvent = () => {
-        dispatch(onDeleteEvent());
+    const startDeletingEvent = async () => {
+
+        try {
+            await calendarApi.delete(`/events/${activeEvent.id}`);
+            dispatch(onDeleteEvent());
+        } catch (error) {
+            console.log(error);
+            Swal.fire('Error al eliminar', error.response.data.msg, 'error');
+        }
     }
 
     const startLoadingEvents = async () => {
