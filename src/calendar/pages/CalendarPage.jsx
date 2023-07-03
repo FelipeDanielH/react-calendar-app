695146ae511969e700801201981bae4340036d6c
Felipe <felipe.daniel.henriquez@gmail.com>
2023-07-03 11:42:25 -0400

    ahora el usuario vera de color diferente los eventos no editables

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -5,11 +5,12 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import { CalendarEvent, CalendarModal, FabAddNew, FabDelete, Navbar } from "../";
 import { localizer, getMessagesES } from '../../helpers';
-import { useUiStore, useCalendarStore } from '../../hooks';
+import { useUiStore, useCalendarStore, useAuthStore } from '../../hooks';
 import { useEffect } from 'react';
 
 export const CalendarPage = () => {
 
+  const { user } = useAuthStore();
   const { openDateModal } = useUiStore();
   const { events, setActiveEvent, startLoadingEvents } = useCalendarStore(); 
   const [isButtonDisabled, setIsButtonDisabled] = useState(false);
@@ -17,8 +18,10 @@ export const CalendarPage = () => {
 
   const eventStyleGetter = (event, start, end, isSelected) => {
 
+    const isMyEvent = ( user.uid === event.user._id) || (user.uid === event.user.uid );
+
     const style = {
-      backgroundColor: '#347Cf7',
+      backgroundColor: isMyEvent ? '#347Cf7' : '#465660',
       borderRadius: '0px',
       opacity: 0.8,
       color: 'white'
