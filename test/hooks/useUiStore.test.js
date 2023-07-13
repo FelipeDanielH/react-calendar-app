38ebe3ad78380fd0da7cbe4b0ce43b6427fba7de
Felipe <felipe.daniel.henriquez@gmail.com>
2023-07-13 17:02:55 -0400

    se agregan pruebas sobre open, close y toggle DateModal

--- test/hooks/useUiStore.test.js
+++ test/hooks/useUiStore.test.js
@@ -1,7 +1,7 @@
-import { renderHook } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
 import { useUiStore } from "../../src/hooks";
 import { Provider } from "react-redux";
-import { store, uiSlice } from "../../src/store";
+import { uiSlice } from "../../src/store";
 import { configureStore } from "@reduxjs/toolkit";
 
 const getMockStore = (initialState) => {
@@ -12,7 +12,7 @@ const getMockStore = (initialState) => {
         preloadedState: {
             ui: { ...initialState }
         }
-    })
+    });
 }
 
 describe('Pruebas sobre useUiStore', () => {
@@ -28,6 +28,57 @@ describe('Pruebas sobre useUiStore', () => {
             isDateModalOpen: false,
             openDateModal: expect.any(Function),
             closeDateModal: expect.any(Function),
-        })
+            toggleDateModal: expect.any(Function),
+        });
+
     });
+
+    test('openDateModal debe cambiar a true la propiedad del store isDateModalOpen', () => {
+        const mockStore = getMockStore({ isDateModalOpen: false });
+
+        const { result } = renderHook(() => useUiStore(), {
+            wrapper: ({ children }) => <Provider store={mockStore}> {children} </Provider>
+        });
+
+        const { openDateModal } = result.current;
+
+        act(() => {
+            openDateModal();
+        });
+
+        expect(result.current.isDateModalOpen).toBeTruthy();
+
+    });
+
+    test('closeDateModal debe cambiar a false la propiedad del store isDateModalOpen', () => {
+        const mockStore = getMockStore({ isDateModalOpen: true });
+
+        const { result } = renderHook(() => useUiStore(), {
+            wrapper: ({ children }) => <Provider store={mockStore}>{children}</Provider>
+        });
+
+        act(() => {
+            result.current.closeDateModal();
+        });
+
+        expect( result.current.isDateModalOpen ).toBeFalsy();
+
+    });
+
+    test(' toggleDateModal debe cambiart el estado actual de la propiedad isDateModalOpen', ()=>{
+        const mockStore = getMockStore({ isDateModalOpen: true });
+
+        const { result }= renderHook( () => useUiStore() , {
+            wrapper: ({ children }) => <Provider store={mockStore}>{ children }</Provider>
+        })
+
+        const { toggleDateModal } = result.current;
+
+        act( () => {
+            toggleDateModal();
+        });
+
+        expect( result.current.isDateModalOpen ).toBeFalsy();
+        
+    })
 });
