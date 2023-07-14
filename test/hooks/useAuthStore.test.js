7faa57f1f370ae6c999b3c4198533c7542915eee
Felipe <felipe.daniel.henriquez@gmail.com>
2023-07-14 08:39:42 -0400

    se agrega prueba sobre el login

--- test/hooks/useAuthStore.test.js
+++ test/hooks/useAuthStore.test.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { AuthSlice } from "../../src/store";
-import { renderHook } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
 import { useAuthStore } from "../../src/hooks";
 import { Provider } from "react-redux";
-import { initialState } from "../fixtures/authStates";
+import { initialState, notAutheticatedState } from "../fixtures/authStates";
+import { testUserCredentials } from "../fixtures/testUser";
 
 const getMockStore = (initialState) => {
     return configureStore({
@@ -19,7 +20,7 @@ const getMockStore = (initialState) => {
 describe('Pruebas sobre authStore', () => {
     test('debe regresar los valores por defecto', () => {
 
-        const mockStore = getMockStore({...initialState});
+        const mockStore = getMockStore({ ...initialState });
 
         const { result } = renderHook(() => useAuthStore(), {
             wrapper: ({ children }) => <Provider store={mockStore}> {children} </Provider>
@@ -35,4 +36,31 @@ describe('Pruebas sobre authStore', () => {
             startRegister: expect.any(Function)
         });
     });
+
+
+    test('startLogin debe de realizar el login correctamente', async () => {
+        localStorage.clear();
+
+        const mockStore = getMockStore({ ...notAutheticatedState });
+
+        const { result } = renderHook(() => useAuthStore(), {
+            wrapper: ({ children }) => <Provider store={mockStore}> {children} </Provider>
+        });
+
+        await act(async () => {
+            await result.current.startLogin(testUserCredentials);
+        });
+
+        const { errorMessage, status, user } = result.current;
+
+        expect({ errorMessage, status, user }).toEqual({
+            errorMessage: undefined,
+            status: 'authenticated',
+            user: { name: 'Test User', uid: '64aae3bebec14cdab18f99f8' },
+        })
+
+        expect( localStorage.getItem('token')).toEqual( expect.any(String));
+        expect(localStorage.getItem('token-init-date')).toEqual( expect.any(String));
+    });
+
 });
